510e4a37e2dae5e3e77ca9a9d78969ac6facc109
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2022-03-30 19:51:27 +0100

    housekeeping

--- bux/utils.py
+++ bux/utils.py
@@ -15,12 +15,6 @@ def is_raspberrypi():
     except Exception: pass
     return False
 
-
-    start_dt = 5
-    filename = os.path.join(path, '%s.h264'%(start_dt))
-    csvname = os.path.join(path, '%s.csv'%(start_dt))
-    print(path)
-
 def list_ports():
     """
     Test the ports and returns a tuple with the available ports and the ones that are working.
@@ -33,16 +27,16 @@ def list_ports():
         camera = cv2.VideoCapture(dev_port)
         if not camera.isOpened():
             non_working_ports.append(dev_port)
-            print("Port %s is not working." %dev_port)
+            # print("Port %s is not working." %dev_port)
         else:
             is_reading, img = camera.read()
             w = camera.get(3)
             h = camera.get(4)
             if is_reading:
-                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                # print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
                 working_ports.append(dev_port)
             else:
-                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+                # print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
                 available_ports.append(dev_port)
         dev_port +=1
     return available_ports,working_ports,non_working_ports
