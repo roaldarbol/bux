f2708c18a33ad1bc35edadf011fc572de6032235
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2022-12-06 14:29:06 +0000

    pre-commit file and re-factoring

--- bux_recorder/BuxController.py
+++ bux_recorder/BuxController.py
@@ -3,8 +3,8 @@ import importlib.util
 from inspect import getmembers, isfunction
 import belay
 
-file_path = '/Users/roaldarbol/MEGA/Documents/sussex/projects/bux-recorder/python/belay_single_script.py'
-module_name = 'belay_single_script'
+file_path = "/Users/roaldarbol/MEGA/Documents/sussex/projects/bux-recorder/python/belay_single_script.py"
+module_name = "belay_single_script"
 
 spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
@@ -12,14 +12,16 @@ spec.loader.exec_module(module)
 
 # check if it's all there..
 fcts = []
+
+
 def bla(mod):
     # for name in dir(mod):
     for o in getmembers(mod):
-        print(o) 
+        print(o)
         if isfunction(o[0]):
             fcts.append(o[0])
+
+
 bla(module)
 
 # ClassName = device.task(ClassName)
-
-
